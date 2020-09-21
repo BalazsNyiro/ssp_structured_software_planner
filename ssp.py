83851fbad8462772b8e27c01e4265f7f4ae1a2dd
Balazs Nyiro <diogenesz@pergamen.hu>
2020-09-21 20:09:49 +0200

    typo

--- ssp.py
+++ ssp.py
@@ -11,7 +11,7 @@ import lib_tkinter
 # Root.mainloop()
 
 
-import tkinter as tk
+import tkinter
 
 CanvasWidget = None
 
@@ -29,12 +29,12 @@ def cmd_empty():
     pass
 def win_main(Prg, CanvasWidth=600, CanvasHeight=400):
 
-    Root = tk.Tk()
+    Root = tkinter.Tk()
     Root.title("title")
 
-    MenuBar = tk.Menu(Root)
+    MenuBar = tkinter.Menu(Root)
 
-    MenuAbout = tk.Menu(MenuBar, tearoff=1)
+    MenuAbout = tkinter.Menu(MenuBar, tearoff=1)
     MenuBar.add_cascade(label='About', menu=MenuAbout)
     MenuAbout.add_command(label="Site", compound='left', command=cmd_empty)
     MenuAbout.add_command(label="Facebook group", compound='left', command=cmd_empty)
@@ -43,28 +43,32 @@ def win_main(Prg, CanvasWidth=600, CanvasHeight=400):
     Root.config(menu=MenuBar)
 
     # placeholder, small distance from left frame
-    tk.Label(Root, text=" ").grid(row=0, column=0, sticky=tk.W)
+    tkinter.Label(Root, text=" ").grid(row=0, column=0, sticky=tkinter.W)
 
     ################################################
     global CanvasWidget
-    CanvasWidget = tk.Canvas(Root, width=CanvasWidth, height=CanvasHeight)
+    CanvasWidget = tkinter.Canvas(Root, width=CanvasWidth, height=CanvasHeight)
     CanvasWidget.bind("<Key>", key)
     CanvasWidget.bind("<ButtonPress-1>", mouse_button_press)
     CanvasWidget.bind("<ButtonRelease-1>", mouse_button_release)
     CanvasWidget.bind("<B1-Motion>", mouse_button_pressed_and_moved)
 
+  
 
     ObjSelected = CanvasWidget.create_rectangle(0, 0, 50, 50, fill="blue")
     ObjSelected = CanvasWidget.create_rectangle(CanvasWidth-50, CanvasHeight-50, CanvasWidth, CanvasHeight, fill="blue")
 
-    CanvasWidget.grid(row=3, column=1, sticky="nsew", columnspan=3) # tk.E
+    CanvasWidget.grid(row=3, column=1, sticky="nsew") # tkinter.E
 
     Root.grid_columnconfigure(1, weight=1)
     Root.grid_rowconfigure(3, weight=1)
 
-    scroll = tk.Scrollbar(Root, command=CanvasWidget.yview)
-    CanvasWidget.configure(yscrollcommand=scroll.set)
-    scroll.grid(row=3, column=3, sticky=tk.NS)
+    ScrollVertical = tkinter.Scrollbar(Root, command=CanvasWidget.yview)
+    ScrollHorizontal = tkinter.Scrollbar(Root, command=CanvasWidget.xview, orient="horizontal")
+    CanvasWidget.configure(yscrollcommand=ScrollVertical.set)
+    CanvasWidget.configure(xscrollcommand=ScrollHorizontal.set)
+    ScrollVertical.grid(row=3, column=2, sticky=tkinter.NS)
+    ScrollHorizontal.grid(row=4, column=1, sticky=tkinter.EW)
     Root.mainloop()
 
 win_main(PrgGlobal)
