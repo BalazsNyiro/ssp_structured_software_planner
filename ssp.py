76e9b9401e4f0a3ecc57e8facf60c2bd367a8024
Balazs Nyiro <diogenesz@pergamen.hu>
2020-09-21 21:32:07 +0200

    not correct but liveable

--- ssp.py
+++ ssp.py
@@ -12,7 +12,7 @@ import lib_tkinter
 
 
 import tkinter
-
+from tkinter import ttk
 CanvasWidget = None
 
 def key(event):
@@ -45,25 +45,35 @@ def win_main(Prg, CanvasWidth=600, CanvasHeight=400):
     # placeholder, small distance from left frame
     tkinter.Label(Root, text=" ").grid(row=0, column=0, sticky=tkinter.W)
 
+    Container = ttk.Frame(Root)
     ################################################
     global CanvasWidget
-    CanvasWidget = tkinter.Canvas(Root, width=CanvasWidth, height=CanvasHeight)
+    CanvasWidget = tkinter.Canvas(Container, width=CanvasWidth, height=CanvasHeight)
     CanvasWidget.bind("<Key>", key)
     CanvasWidget.bind("<ButtonPress-1>", mouse_button_press)
     CanvasWidget.bind("<ButtonRelease-1>", mouse_button_release)
     CanvasWidget.bind("<B1-Motion>", mouse_button_pressed_and_moved)
 
-  
+    ScrollableFrame = ttk.Frame(CanvasWidget)
+    ScrollableFrame.bind(
+        "<Configure>",
+        lambda e: CanvasWidget.configure(
+            scrollregion=CanvasWidget.bbox("all")
+        )
+    )
+    CanvasWidget.create_window((0, 0), window=ScrollableFrame, anchor="nw")
 
     ObjSelected = CanvasWidget.create_rectangle(0, 0, 50, 50, fill="blue")
     ObjSelected = CanvasWidget.create_rectangle(CanvasWidth-50, CanvasHeight-50, CanvasWidth, CanvasHeight, fill="blue")
 
-    CanvasWidget.grid(row=3, column=1, sticky="nsew") # tkinter.E
+    CanvasWidget.grid() # tkinter.E
+    Container.grid(row=3, column=1, sticky="nsew") # tkinter.E
 
+    # cell resizabe, flexible:
     Root.grid_columnconfigure(1, weight=1)
     Root.grid_rowconfigure(3, weight=1)
 
-    ScrollVertical = tkinter.Scrollbar(Root, command=CanvasWidget.yview)
+    ScrollVertical = tkinter.Scrollbar(Root, command=CanvasWidget.yview, orient="vertical")
     ScrollHorizontal = tkinter.Scrollbar(Root, command=CanvasWidget.xview, orient="horizontal")
     CanvasWidget.configure(yscrollcommand=ScrollVertical.set)
     CanvasWidget.configure(xscrollcommand=ScrollHorizontal.set)
